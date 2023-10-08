8231aed4e3f27e47895958e2775b9a0bd68f696d
Sohan jagadeesh <sjag0009@student.monash.edu>
2023-10-09 03:01:03 +1100

    copied backend files

---
+++ index.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import OpenAI from 'openai';
+import cors from 'cors'; // Import the cors middleware
+
+const app = express();
+const port = 3000;
+
+app.use(cors()); // Use the cors middleware to handle CORS
+app.use(express.json()); // Middleware to parse JSON requests
+
+const openai = new OpenAI({
+  apiKey: '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',
+});
+
+app.post('/poem', async (req, res) => { // Change to POST
+  try {
+    const promptText = req.body.prompt; // Extract the prompt from the request body
+
+    const stream = await openai.chat.completions.create({
+      model: 'gpt-3.5-turbo',
+      messages: [{ role: 'user', content: promptText }],
+      stream: true,
+    });
+
+    res.setHeader('Content-Type', 'text/plain');
+    for await (const part of stream) {
+      res.write(part.choices[0]?.delta?.content || '');
+    }
+    res.end();
+  } catch (error) {
+    res.status(500).send('Error processing request.');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
